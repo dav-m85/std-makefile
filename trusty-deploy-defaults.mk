e8fd0d8515acdab56ecb0f65a4827f7e56f0fff0
David Moreau <dav.m85@gmail.com>
2016-12-20 14:24:17 -0500

    add cron and clean support

--- trusty-deploy-defaults.mk
+++ trusty-deploy-defaults.mk
@@ -1,6 +1,17 @@
-REPLACE_VAR ?= CD
+ENV ?=
+
+NGINX_PREFIX ?= $(ENV)
+NGINX_DIST ?= dist/nginx
 NGINX_ENABLED ?= /etc/nginx/sites-enabled
 NGINX_AVAILABLE ?= /etc/nginx/sites-available
-DIST ?= dist/nginx.conf
-SERVER ?= default_server
-# SECURE do not define this unless you want secure mode all the time.
+NGINX_REPLACE_VAR ?= CD NGINX_PREFIX
+
+SUPERVISOR_PREFIX ?= $(ENV)
+SUPERVISOR_DIST ?= dist/supervisor
+SUPERVISOR_D ?= /etc/supervisor/conf.d
+SUPERVISOR_REPLACE_VAR ?= CD SUPERVISOR_PREFIX
+
+CRON_PREFIX ?= $(ENV)
+CRON_DIST ?= dist/cron
+CRON_D ?= /etc/cron.d
+CRON_REPLACE_VAR ?= CD CRON_PREFIX
--- trusty-deploy.mk
+++ trusty-deploy.mk
@@ -11,15 +11,68 @@ is_sudo:
 	echo Make sure i am sudo or fail
 
 #
-# Deploy nginx host
+# nginx
 #
-std_deploy_nginx:
-	cp $(DIST) $(NGINX_AVAILABLE)/$(NGINX_NAME)
-ifdef SECURE
-	echo "SECURE mode. Variable replacement hidden from logs for: $(REPLACE_VAR)"
-	@$(foreach var,$(REPLACE_VAR),sed -i "s/__$(var)__/$(subst /,\/,$($(var)))/g" $(NGINX_AVAILABLE)/$(NGINX_NAME);)
-else
-	$(foreach var,$(REPLACE_VAR),sed -i "s/__$(var)__/$(subst /,\/,$($(var)))/g" $(NGINX_AVAILABLE)/$(NGINX_NAME);)
-endif
-	[ -e $(NGINX_ENABLED)/$(NGINX_NAME) ] || ln -s $(NGINX_AVAILABLE)/$(NGINX_NAME) $(NGINX_ENABLED)/$(NGINX_NAME)
+std_deploy_nginx: std_install_nginx
+	echo "std_deploy_nginx is deprecated, please use std_install_nginx instead"
+
+std_install_nginx:
+	@for F in `find $(NGINX_DIST) -type f -printf "%f\n"`; do \
+		echo "Copying $$F"; \
+		cp -- "$(NGINX_DIST)/$$F" "$(NGINX_AVAILABLE)/$(NGINX_PREFIX)$$F"; \
+		[ -e $(NGINX_ENABLED)/$(NGINX_PREFIX)$$F ] || ln -s $(NGINX_AVAILABLE)/$(NGINX_PREFIX)$$F $(NGINX_ENABLED)/$(NGINX_PREFIX)$$F; \
+	done;
+	@for F in `find $(NGINX_DIST) -type f -printf "%f\n"`; do \
+	  echo "Replacing $$F with: $(NGINX_REPLACE_VAR)"; \
+	  $(foreach var,$(NGINX_REPLACE_VAR),sed -i "s/__$(var)__/$(subst /,\/,$($(var)))/g" $(NGINX_AVAILABLE)/$(NGINX_PREFIX)$$F;) \
+	done;
 	service nginx reload
+
+std_clean_nginx:
+	@for F in `find $(NGINX_DIST) -type f -printf "%f\n"`; do \
+		echo "Removing $$F"; \
+		rm "$(NGINX_ENABLED)/$(NGINX_PREFIX)$$F"; \
+	done;
+	service nginx reload
+
+#
+# supervisor
+#
+std_install_supervisor:
+	@for F in `find $(SUPERVISOR_DIST) -type f -printf "%f\n"`; do \
+		echo "Copying $$F"; \
+		cp -- "$(SUPERVISOR_DIST)/$$F" "$(SUPERVISOR_D)/$(SUPERVISOR_PREFIX)$$F"; \
+	done;
+	@for F in `find $(SUPERVISOR_DIST) -type f -printf "%f\n"`; do \
+	  echo "Replacing $$F with: $(SUPERVISOR_REPLACE_VAR)"; \
+	  $(foreach var,$(SUPERVISOR_REPLACE_VAR),sed -i "s/__$(var)__/$(subst /,\/,$($(var)))/g" $(SUPERVISOR_D)/$(SUPERVISOR_PREFIX)$$F;) \
+	done;
+	service supervisor force-reload
+
+std_clean_supervisor:
+	@for F in `find $(SUPERVISOR_DIST) -type f -printf "%f\n"`; do \
+		echo "Removing $$F"; \
+		rm "$(SUPERVISOR_D)/$(SUPERVISOR_PREFIX)$$F"; \
+	done;
+	service supervisor force-reload
+
+#
+# cron
+#
+std_install_cron:
+	@for F in `find $(CRON_DIST) -type f -printf "%f\n"`; do \
+		echo "Copying $$F"; \
+		cp -- "$(CRON_DIST)/$$F" "$(CRON_D)/$(CRON_PREFIX)$$F"; \
+	done;
+	@for F in `find $(CRON_DIST) -type f -printf "%f\n"`; do \
+	  echo "Replacing $$F with: $(CRON_REPLACE_VAR)"; \
+	  $(foreach var,$(CRON_REPLACE_VAR),sed -i "s/__$(var)__/$(subst /,\/,$($(var)))/g" $(CRON_D)/$(CRON_PREFIX)$$F;) \
+	done;
+	service cron restart
+
+std_clean_cron:
+	@for F in `find $(CRON_DIST) -type f -printf "%f\n"`; do \
+		echo "Removing $$F"; \
+		rm "$(CRON_D)/$(CRON_PREFIX)$$F"; \
+	done;
+	service cron restart
